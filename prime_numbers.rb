95766b7b94611855f3c11895314613ef1cde53af
Jun Wan Goh <gohjunwan@gmail.com>
2016-06-11 00:28:49 +0800

    [Feature] Add sieve of eratosthenes to find prime numbers

--- prime_numbers.rb
+++ prime_numbers.rb
@@ -31,6 +31,36 @@ def sum_of_prime_numbers(n)
   sum
 end
 
+def sieve_of_eratosthenes(n, nth)
+  primes = [true] * (n + 1)
+  primes[0] = primes[1] = false
+
+  i = 2
+
+  while i * i <= n
+    k = i * i
+    while k <= n
+      primes[k] = false
+      k += i
+    end
+    i += 1
+  end
+
+  sum = 0
+  count = 0
+  primes.each_with_index do |prime, number|
+    if prime
+      sum += number
+      count += 1
+    end
+
+    return sum if count >= nth
+  end
+
+  sum
+end
+
 # https://primes.utm.edu/lists/small/1000.txt
 puts sum_of_prime_numbers(1000)
+puts sieve_of_eratosthenes(8000, 1000)
 
